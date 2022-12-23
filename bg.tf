8ab8e777e4e39594f63b215be5871863253c1b83
Kranthiuppari <kranthi@wisig.com>
2022-12-23 22:12:26 +0530

    remote storage account

---
+++ bg.tf
@@ -0,0 +1,8 @@
+terraform {
+backend "azurerm"{
+storage_account_name ="storage0327"
+container_name ="container1"
+key = "terraform.tfstate"
+access_key ="N0owiMqYmjrzYlcsJkj3+Mn9QioawHyy+DUup3AxTF63MquBQB5u3lYg4lKH4QI3dXY/+9ALS8Bb+ASt6QOxIA=="
+}
+}
